247abfe3c3b5ccaf7edbc41fa06f16b6b97fe860
ChangyuLee <lck3365@kaist.ac.kr>
2024-12-11 11:27:16 +0900

    fix : change select title

--- content.js
+++ content.js
@@ -16,7 +16,7 @@
   // 선택 제목 생성
   const selectTitle = document.createElement('div');
   selectTitle.id = 'philosopher-select-title-container';
-  selectTitle.innerText = 'Select Talker';
+  selectTitle.innerText = 'Select Philosophers';
 
   // 이미지 컨테이너 생성
   const imageContainer = document.createElement('div');
